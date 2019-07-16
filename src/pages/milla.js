80124632d7c87d5d346599ff21b3dfd2715cff58
lorio <lori@loriortiz.com>
2019-07-16 12:35:03 -0400

    Blockquote and style edits

--- src/pages/milla.js
+++ src/pages/milla.js
@@ -71,12 +71,13 @@ const MillaPage = () => (
           <StyledBackgroundSection Tag="section"
             fluid={data.millaImage.childImageSharp.fluid}
             backgroundColor={`#fff7f0`}
+            alt={`Milla performing`}
           >
-          <Headline>Milla<br />Jovovich<br />Plays a part<br />On runway<br /> record and film</Headline>
+          <Headline>Milla<br />Jovovich<br />Plays a part<br />On runway<br />Record and film</Headline>
         </StyledBackgroundSection>
-        <Caption>Milla, 1995. Director:Jan Dikkers; Make-up: Michael Del Fino; Shot at Sun Studios, NYC</Caption>
+        <Caption><em><b>Milla</b></em>. Director:Jan Dikkers; Make-up: Michael Del Fino; Shot at Sun Studios, NYC</Caption>
         <ArticleTitle><em>ROLE MODEL</em></ArticleTitle>
-        <p>By <strong>Rosemary Grillo</strong>. Photo: <strong>Stephen Churchill Downes</strong></p>
+        <p>By <strong>Rosemary Grillo</strong>. Photo: <strong>Stephen Churchill Downes</strong>; In: <em><b>COVER</b></em> Summer 1995</p>
         <p>Milla was a model at eleven, and a world traveler, and <em>haute-couture femme fatale</em> by thirteen. Her mother, a Ukrainian actress, and her father, a Yugoslavian doctor, had left the USSR when Milla was five. The family landed in Southern California where Milla became the existentialist, reading Balzac and Latin American lit from Allende to Marquez, and partying from modeling shoots to movie sets.</p>
         <p>(Remember <em>Return to the Blue Lagoon</em> and Richard Linklater's recent <em>Dazed and Confused</em>?)</p>
         <p>Now a seasoned star at nineteen, Milla describes her mother as a long-standing influence. She wanted to follow in her mother's footsteps and become an actress. One thing lead to another and Milla found herself at twelve made up to look twenty. Being in front of the lens became natural.</p>
@@ -85,7 +86,7 @@ const MillaPage = () => (
         <p>Her first album, <em>The Divine Comedy</em>, had lyric resonance. "I'm a writer and a performer. My lyrics are like my journal. I don't have the skill of a musician; the people I work with have devoted their whole lives to it and we're perfectly in sync. I don't want to be an ego and say I do everything.</p>
         <p>"I love working with the band as a collective. I hate to be dictated to by a producer. At this point I've put so much into learning to perform and focus on the music, that I could do it forever, but I can't be totally dependent on my record company.</p>
         <p>“Look, I love modeling. Its a blast to work with everyone. I just shot here last week with Testino Magni for <em>Italian Vogue</em>. It was great to see old friends after not working for a while, like Testino who's known me since I was a kid."</p>
-        <p>Milla’s definitely on her way up in the world but her dreams about flying are most revealing. "I have these sensual dreams about Courtney: she's usually smiling. I know it's cheesy but she's so beautiful, cool, awesome - a goddess. I'm in love with her," she bursts out, jumping up from her director's chair. But would she like to meet? "No, I wouldn't want to break the pedestal I've put her on."</p>
+        <p>Milla’s definitely on her way up in the world but her dreams about flying are most revealing. "I have these sensual dreams about Courtney [Love]: she's usually smiling. I know it's cheesy but she's so beautiful, cool, awesome — a goddess. I'm in love with her," she bursts out, jumping up from her director's chair. But would she like to meet? "No, I wouldn't want to break the pedestal I've put her on."</p>
         </Article>
         <HomeLink to="/">COVER</HomeLink>
       </Layout>

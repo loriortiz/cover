373cb0aff1a3746b8cc72ff9e37e225d8668470d
lorio <lori@loriortiz.com>
2019-09-30 18:23:37 -0400

    Add portrait reed page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Link } from "gatsby"
 import SEO from "../components/seo"
 
 const ArticlesPage = () => (
   <Layout>
     <SEO title="Articles" />
     <h1>Articles</h1>
-    <p>List of articles TK</p>
+    <p><Link to="/reed-portrait">Timothy Greenfield-Sanders’ portrait diary</Link> of his year directing and producing <em>Lou Reed: Rock and Roll Heart</em> for PBS.</p>
   </Layout>
 )
 

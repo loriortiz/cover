4142ee1b2e7bc1acbebb548a4a3046da5ac78f71
lorio <lori@loriortiz.com>
2019-10-01 11:12:58 -0400

    Complete articles page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -7,7 +7,15 @@ const ArticlesPage = () => (
   <Layout>
     <SEO title="Articles" />
     <h1>Articles</h1>
-    <p><Link to="/reed-portrait">Timothy Greenfield-Sanders’ portrait diary</Link> of his year directing and producing <em>Lou Reed: Rock and Roll Heart</em> for PBS.</p>
+    <p><Link to="/reed-portrait">Timothy Greenfield-Sanders’ portrait diary</Link> of his year (1998) directing and producing <em>Lou Reed: Rock and Roll Heart</em> for PBS and filming Reed and Robert Wilson's <em>Time Rocker</em>.</p>
+
+    <p><Link to="/andrei-coddrescu">Andrei Codrescu</Link> discusses his new (1995) novel <em>Blood Countess</em>, his first under his own name.</p>
+
+    <p><Link to="/milla">Milla: On Runway, Record, and Film.</Link> A 1995 interview with the ubiquitous artist.</p>
+
+    <p><Link to="/kiki-smith">Kiki Smith</Link> on her 1998 Mattress Factory Installation.</p>
+
+    <p>Do you have a favorite <em>COVER</em> article you'd like to see re<em>COVER</em>ed? <Link to="/contact">Drop us a line.</Link></p>
   </Layout>
 )
 
